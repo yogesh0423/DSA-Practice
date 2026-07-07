cbdb4e88d96bc40edc95873ba8a1009b2b3d92ce
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-07-07 17:10:00 +0000

    Solved nested list problem of python

---
+++ Basics/NestedLists.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())  
+        students.append([score, name ])
+     
+    students.sort()
+    studentsCopy = []
+    for i in range(len(students)):
+        studentsCopy.append(students[i][0])
+    
+    arr = list(dict.fromkeys(studentsCopy)) 
+    
+    for i in range(len(students)):
+        if students[i][0] == arr[1]:
+            print(students[i][1])
