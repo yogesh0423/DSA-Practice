9cee13ba534dd5ba0dac99fce9a38ba45e0b23e2
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-07-12 19:28:39 +0000

    Updated the code of PrintNum file

--- Basics/Loops/While/PrintNum.py
+++ Basics/Loops/While/PrintNum.py
@@ -1,6 +1,22 @@
-
+#Program 1
 i = 5 
 
 while i >= 5 and i < 21: 
     print(i)
     i += 1 
+
+
+#Program 2
+a = 10
+
+while a < 11 and a >= 0:
+    print(a)
+    a -= 1
+    
+
+#Program 3
+a = 11
+
+while a<20:
+    print(a)
+    a = a + 2
