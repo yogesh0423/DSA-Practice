f9f5b6bde5b3bff37d4ad38f853d95c7eadc2f6d
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-07-14 21:07:32 +0530

    Add multiple programs for basic operations in Part1.py

--- Basics/Revision/Part1.py
+++ Basics/Revision/Part1.py
@@ -1,1 +1,28 @@
+#Program 1
 print("Hello, world!")
+
+#Program 2
+a = int(input())
+b = int(input())
+c = a+b
+print(c)
+
+#Program 3
+a = int(input())
+b = 
+
+
+#Program 4
+base = int(input())
+height = int(input())
+area = 0.5 * base * height
+print(area)
+
+#Program 6
+a = 10 
+b = 5
+temp = 0
+b = temp 
+a = b
+print(a)
+print(b)
