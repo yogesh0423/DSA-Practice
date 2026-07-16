70436d3042acd856a3ec92decf97d0705f27a4a8
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-07-16 07:34:14 +0530

    Add largest of three numbers functionality
    Added functionality to find the largest of three numbers.

--- Basics/Revision/Part2.py
+++ Basics/Revision/Part2.py
@@ -1,7 +1,18 @@
-#Program 1
+------Even Odd------
 n = int(input())
 if n % 2 == 0:
   print("Even")
 else:
   print("Odd")
   
+------Largest of three------
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a > b and a > c:
+  print("a is largest elem")
+elif b > a and b > c:
+  print("b is largest elem")
+elif c > a and c > b:
+  print("c is largest elem")
